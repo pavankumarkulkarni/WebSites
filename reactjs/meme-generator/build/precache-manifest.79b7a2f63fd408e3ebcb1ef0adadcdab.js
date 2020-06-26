3de01ed86772d3690b11de0366ce900f34a58772
Pavan kumar Kulkarni <pavankumar.kulkarni@gmail.com>
2020-06-25 22:38:46 -0400

    Add files via upload

---
+++ reactjs/meme-generator/build/precache-manifest.79b7a2f63fd408e3ebcb1ef0adadcdab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d2fa46280c2421769c7bf2c76d8687",
+    "url": "/index.html"
+  },
+  {
+    "revision": "540c2a65dc4eda66a10e",
+    "url": "/static/css/main.9502c16b.chunk.css"
+  },
+  {
+    "revision": "4831f0b1bfc17b3266e8",
+    "url": "/static/js/2.02143386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02143386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "540c2a65dc4eda66a10e",
+    "url": "/static/js/main.018a6a8d.chunk.js"
+  },
+  {
+    "revision": "3ee6d9289089d700c3ad",
+    "url": "/static/js/runtime-main.2d4e65bc.js"
+  }
+]);
